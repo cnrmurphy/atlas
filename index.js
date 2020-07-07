836f78bd922be20099ac732cad4b8e4272ffac8b
cjm322 <conor.murphy@gmail.com>
2020-07-07 11:56:09 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { FTXUS } = require('ftx-us');
+
+const apiKey = process.env.FTX_API_KEY;
+const apiSecret = process.env.FTX_API_SECRET;
+
+const ftxUs = new FTXUS({ key: apiKey, secret: apiSecret });
+
+(async () => {
+  try { 
+    const wallet = ftxUs.Wallet;
+    let lastchecked = Date.now();
+    console.log(lastchecked)
+    const balances = await wallet.getBalances();
+    console.log(balances);
+  } catch(e) {
+    console.log(e);
+  }
+})();
