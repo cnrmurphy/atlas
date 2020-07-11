fd73d4c040b1d28eebc4745bf0c66cb195b742b2
8bitstout <conor.murphy4304@gmail.com>
2020-07-10 21:04:20 -0400

    More trade logic

--- TransferService.js
+++ TransferService.js
@@ -20,8 +20,24 @@ class TransferService {
     return despositInfo.address;
   }
 
-  async sendToFtx(coin, amount) {
+  async getFtxAddress(coin) {
+    const resp = await this._ftxClient.Wallet.getDepositAddress(coin);
+    return resp;
+  }
 
+  async sendToFtx(coin, amount) {
+    const { address } = await this.getFtxAddress(coin);
+    if (!address) {
+      throw new Error(`Could not retrieve a wallet address for ${coin} on FTX!`);
+    }
+    console.log(address);
+    console.log(`Sending ${amount} ${coin} to FTX using address, ${address}`);
+    const resp = await this._coinbaseClient.withdrawCrypto({
+      amount,
+      currency: coin,
+      crypto_address: address
+    });
+    return resp;
   }
 
   async sendToCoinbase(coin, amount) {
@@ -30,6 +46,8 @@ class TransferService {
       throw new Error(`Could not retrieve a wallet address for ${coin} on Coinbase!`);
     }
     console.log(`Sending ${amount} ${coin} to Coinbase using address, ${address}`);
+    const resp = await this._ftxClient.Wallet.requestWithdrawl(coin, amount, address);
+    return resp;
   }
 
   async convertCoinbaseCoin({ from=null, to=null, amount=0 }) {
