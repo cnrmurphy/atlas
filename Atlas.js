0bcd62f34d910f099706993b536394d281fa46d3
8bitstout <conor.murphy4304@gmail.com>
2020-07-10 22:52:39 -0400

    changed db to prisma

--- Atlas.js
+++ Atlas.js
@@ -3,11 +3,11 @@ const SpatialArbitrageService = require('./SpatialArbitrageService');
 const TransferService = require('./TransferService');
 
 class Atlas {
-  constructor(coinbaseClient, ftxClient, db) {
+  constructor(coinbaseClient, ftxClient, prisma) {
     this._coinbaseClient = coinbaseClient;
     this._ftxClient = ftxClient;
     this._transferService = new TransferService(coinbaseClient, ftxClient);
-    this._spatialArbitrageService  = new SpatialArbitrageService(coinbaseClient, ftxClient, this._transferService, db);
+    this._spatialArbitrageService  = new SpatialArbitrageService(coinbaseClient, ftxClient, this._transferService, prisma);
   }
 
   get SpatialArbitrage() {
